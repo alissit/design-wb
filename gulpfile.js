def11eb6a2a6998cb5b75019c038f9723b9b1735
Alissa <alissit@gmail.com>
2018-02-27 15:49:38 +0000

    Ready to start html

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp 			= require("gulp"),
+	watch 			= require("gulp-watch"),
+	postcss 		= require("gulp-postcss"),
+	autoprefixer 	= require("autoprefixer"),
+	cssvars			= require("postcss-simple-vars"),
+	nested			=require("postcss-nested"),
+	browserSync		=require("browser-sync").create();
+
+
+gulp.task("html", function(){
+
+	console.log("HTML change");
+});
+
+gulp.task("styles", function(){
+	gulp.src("./app/assets/styles/styles.css")
+		.pipe(postcss([nested, cssvars, autoprefixer]))
+		.pipe(gulp.dest("./app/temp/styles"));
+});
+
+
+
+gulp.task("watch", function(){
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch("./app/index.html", function(){
+		browserSync.reload();
+	});
+
+	watch("./app/assets/styles/**/*.css", function(){
+		gulp.start("cssInject");	
+	});
+});
+
+gulp.task("cssInject", ["styles"], function(){
+	return gulp.src("./app/assets/styles/styles.css")
+		.pipe(browserSync.stream());
+})
