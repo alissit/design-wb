91f6c3825a1d7eb63035ec6bf89af9efba67fc01
Alissa <alissit@gmail.com>
2018-03-01 20:45:02 +0000

    Shaping the landing page

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,5 @@
-var gulp 			= require("gulp"),
-	watch 			= require("gulp-watch"),
-	postcss 		= require("gulp-postcss"),
-	autoprefixer 	= require("autoprefixer"),
-	cssvars			= require("postcss-simple-vars"),
-	nested			=require("postcss-nested"),
-	browserSync		=require("browser-sync").create();
+require("./gulp/tasks/styles");
+require("./gulp/tasks/watch");
+	
 
 
-gulp.task("html", function(){
-
-	console.log("HTML change");
-});
-
-gulp.task("styles", function(){
-	gulp.src("./app/assets/styles/styles.css")
-		.pipe(postcss([nested, cssvars, autoprefixer]))
-		.pipe(gulp.dest("./app/temp/styles"));
-});
-
-
-
-gulp.task("watch", function(){
-
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
-
-	watch("./app/index.html", function(){
-		browserSync.reload();
-	});
-
-	watch("./app/assets/styles/**/*.css", function(){
-		gulp.start("cssInject");	
-	});
-});
-
-gulp.task("cssInject", ["styles"], function(){
-	return gulp.src("./app/assets/styles/styles.css")
-		.pipe(browserSync.stream());
-})
