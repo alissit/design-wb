6650763286296fb2131115f1bec5f049d9c264a1
Alissa <alissit@gmail.com>
2018-03-10 21:01:31 +0000

    finished scrolling feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import MobileMenu from "./modules/MobileMenu";
 import StickyHeader from "./modules/StickyHeader";
+import RevealOnScroll from "./modules/RevealOnScroll";
+
+var revealOnScroll = new RevealOnScroll();
 
 var mobileMenu = new MobileMenu();
 var stickyheader = new StickyHeader();
